c5c451ebe03467b2d161f6a7d39bf5e68de83b2a
Anurag Bhandari <anurag.bhd@gmail.com>
2019-09-03 15:43:54 +0530

    Implement the story flow
    * Add conditionals in nextStory() as per the story plan
    * Wrap calls to nextStory() in setState()

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -47,14 +47,26 @@ class StoryBrain {
     return _storyData[_storyNumber].choice2;
   }
 
-  void nextStory(int choiceNumber) {}
+  void nextStory(int choiceNumber) {
+    if (_storyNumber == 0) {
+      if (choiceNumber == 1)
+        _storyNumber = 2;
+      else if (choiceNumber == 2) _storyNumber = 1;
+    } else if (_storyNumber == 1) {
+      if (choiceNumber == 1)
+        _storyNumber = 2;
+      else if (choiceNumber == 2) _storyNumber = 3;
+    } else if (_storyNumber == 2) {
+      if (choiceNumber == 1)
+        _storyNumber = 5;
+      else if (choiceNumber == 2) _storyNumber = 4;
+    }
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
 //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
